115291a8541d6c5376ed90dc2ed1fd786669c597
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-03-25 12:10:43 +0100

    Allow passing arbitrary env vars to agent

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,13 @@ async function run(): Promise<void> {
     const containerName: string = core.getInput('container_name', {required: true})
     const apiKey: string = core.getInput('api_key', {required: true})
     const site: string = core.getInput('datadog_site', {required: true})
+    const passthrough_env: string = core.getInput('extra_env', {required: false})
+    const extra_env: string[] = passthrough_env
+      ? passthrough_env.split(',').map((key_value: string) => key_value.trim())
+      : []
 
     core.info('Starting agent')
-    let code = await startAgent(imageName, containerName, apiKey, site)
+    let code = await startAgent(imageName, containerName, apiKey, site, extra_env)
     if (code !== 0) throw new Error(`could not start agent: (${code})`)
 
     code = 1
