701fc6312906a71779be532cef06aee5434e3211
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-03-29 10:17:27 +0100

    Support window runners

--- src/post.ts
+++ src/post.ts
@@ -1,11 +1,18 @@
 import * as core from '@actions/core'
-import {stopAgent} from './stop'
+import {stopAgent, stopWindowsAgent} from './stop'
 
 async function run(): Promise<void> {
   try {
     const containerName: string = core.getInput('container_name', {required: true})
     core.info('Stopping agent')
-    const code = await stopAgent(containerName)
+
+    let code = 0
+    if (process.env['RUNNER_OS'] === 'Windows') {
+      code = await stopWindowsAgent(containerName)
+    } else {
+      code = await stopAgent(containerName)
+    }
+
     if (code !== 0) throw new Error(`could not stop agent (${code})`)
     core.info('Agent stopped')
   } catch (error) {
