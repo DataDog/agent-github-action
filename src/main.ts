e6ee56f84cdc8c0f6441843d15a7ce9f5c67a1bf
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-05-10 09:09:37 +0100

    Inputs for configuring poll initial wait time and max retires (#110)
    * Inputs for configuring poll initial wait time and max retires
    
    * Build action
    
    * Fix typo
    
    * Rebuild

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function run(): Promise<void> {
     const site: string = core.getInput('datadog_site', {required: true})
     const passthrough_env: string = core.getInput('extra_env', {required: false})
     const extra_env: string[] = passthrough_env ? passthrough_env.split(',').map((envvar: string) => envvar.trim()) : []
+    const maxRetries: number = parseInt(core.getInput('max_retries', {required: true}))
+    const initialWaitTime: number = parseInt(core.getInput('initial_wait_time', {required: true}))
 
     core.info('Starting agent')
     let code = await startAgent(imageName, containerName, apiKey, site, extra_env)
@@ -16,13 +18,13 @@ async function run(): Promise<void> {
 
     code = 1
     let attempts = 0
-    while (code !== 0 && attempts < 10) {
+    while (code !== 0 && attempts < maxRetries) {
       attempts++
       core.info('checking agent health')
       code = await getAgentHealth(containerName)
       if (code !== 0) {
-        core.info(`the agent is not ready waiting ${5 * attempts} seconds`)
-        await new Promise(f => setTimeout(f, 5000 * attempts))
+        core.info(`the agent is not ready waiting ${initialWaitTime * attempts} seconds`)
+        await new Promise(f => setTimeout(f, initialWaitTime * 1000 * attempts))
       }
     }
 
